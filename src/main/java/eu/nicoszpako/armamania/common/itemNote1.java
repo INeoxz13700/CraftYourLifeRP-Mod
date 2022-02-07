fafa08d90419555ae17e926e10cb2b7e512b243b
INeoxz13700 <75746428+INeoxz13700@users.noreply.github.com>
2022-02-07 13:05:11 +0100

    Ajout et modification au niveau des assets

---
+++ src/main/java/eu/nicoszpako/armamania/common/itemNote1.java
@@ -0,0 +1,11 @@
+package eu.nicoszpako.armamania.common;
+
+import net.minecraft.item.Item;
+
+public class itemNote1 extends Item {}
+
+
+/* Location:              D:\Devs\deobfuscator\ArmaMania-deobf.jar!\eu\nicoszpako\armamania\common\itemNote1000.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       1.1.3
+ */
